768c96e5a4b546ea1337d4abb7b31f4d1309cc9c
matte_lisi <matteo_lisi@yahoo.it>
2018-12-07 15:05:29 +0000

    fixed bootFooCI

--- R/bootfooci.R
+++ R/bootfooci.R
@@ -1,16 +1,16 @@
 #' Miscellaneous helpers
 #'
 #' Bootstrapped 95% CI for arbitratry function foo (BCA / percentile method)
-#' @param FUN is the function that compute the statistic of interest
+#' @param foo is the function that compute the statistic of interest
 #' @param nsim number of bootstrap simulations (default is 1000)
 #' @param method Either "bca" (default, accellerated & bias-corrected, Efron, 1987), or "percentile"
 #' @param alpha Either "bca" (accellerated & bias-corrected, Efron, 1987), or "percentile"
 #' @return the 95% confidence limits
 #' @export
 
-bootFooCI <- function(v,nsim=1000,FUN=mean(), method="bca", alpha=0.05, ...){
-  FUN <- if (!is.null(FUN)) {match.fun(FUN)}
-	bootFoo <- function(v,i) FUN(v[i],...)
+bootFooCI <- function(v,nsim=1000,foo="mean", method="bca", alpha=0.05, ...){
+  foo <- match.fun(foo)
+	bootFoo <- function(v,i) foo(v[i],...)
 	bootRes <- boot::boot(v,bootFoo,nsim)
 
 	if(method=="percentile"){
@@ -22,7 +22,7 @@ bootFooCI <- function(v,nsim=1000,FUN=mean(), method="bca", alpha=0.05, ...){
 	    # estimate bias in std. norm deviates
 			# The bias-correction parameter is related to the proportion of
 			# bootstrap estimates that are less than the observed statistic
-	    obs <- FUN(v)
+	    obs <- foo(v)
 	    b <- qnorm((sum(bootRes$t > obs)+sum(bootRes$t==obs)/2)/nsim)
 
 	    # estimate acceleration parameter
